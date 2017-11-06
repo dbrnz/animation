2cb68534a960666259d6f2651fdb68a141f11d5d
David Brooks <dave@bcs.co.nz>
2017-11-06 19:18:56 +1300

    Get cell membrane ion channel animation working.

--- animation.py
+++ animation.py
@@ -3,7 +3,7 @@ from lxml import etree as ET
 import numpy as np
 
 import matplotlib
-matplotlib.use('svg')  ## Otherwise transforms are wrong with tight-layout...
+##matplotlib.use('svg')  ## Otherwise transforms are wrong with tight-layout...
 
 from matplotlib import pyplot as plt
 
@@ -13,6 +13,38 @@ from matplotlib.transforms import Affine2D
 
 
 POINTS_PER_INCH = 72.0
+
+# Diagram SVG definitions
+
+DIAGRAM_ANIMATION_SVG = '''
+  <defs xmlns:xlink="http://www.w3.org/1999/xlink">
+    <g id="IonCurrent">
+      <circle cy="0"   r="15"/>
+      <circle cy="30"  r="15"/>
+      <circle cy="60"  r="15"/>
+      <circle cy="90"  r="15"/>
+      <circle cy="120" r="15"/>
+      <circle cy="150" r="15"/>
+      <circle cy="180" r="15"/>
+    </g>
+    <linearGradient x1="0" x2="0" y1="0" y2="200" gradientUnits="userSpaceOnUse" id="IonMaskGradient">
+      <stop offset="0" stop-color="white" stop-opacity="0.1"/>
+      <stop offset="0.5" stop-color="white" stop-opacity="1"/>
+      <stop offset="1" stop-color="white" stop-opacity="1"/>
+    </linearGradient>
+    <mask width="20" height="200" x="0" y="0" maskmaskUnits="userSpaceOnUse" id="IonMask">
+      <ellipse cy="100" rx="40" ry="110" fill="url(#IonMaskGradient)"/>
+    </mask>
+    <clipPath id="IonMaskClipPath">
+      <ellipse cy="100" rx="40" ry="100"/>
+    </clipPath>
+    <g id="IonChannel">
+      <g mask="url(#IonMask)">
+        <use xlink:href="#IonCurrent"/>
+      </g>
+    </g>
+  </defs>
+'''
 
 # Embedded Javascript
 
@@ -30,6 +62,38 @@ ANIMATION_SCRIPT = '''
       }
       inverse(x) {
         return (x - this.offset)/this.scale;
+      }
+    }
+
+    class IonChannel {
+      constructor(id) {
+        this.channel = svg.getElementById(id);
+        this.last_time = 0;
+        this.offset = -15;
+      }
+      paint(time, current) {
+        if (this.channel) {
+          var dt = time - this.last_time;  // seconds
+          if (dt > 0.0) {
+            this.offset += 300*dt*current; // current == 1.0  ==> offset changes by 30px in 100ms
+            if (this.offset > 15.0) this.offset = -15.0;
+            this.channel.setAttribute("y", this.offset);
+          }
+        this.last_time = time;
+        }
+      }
+    }
+
+    class Visualiser {
+      constructor(id, radius_fn) {
+        this.visualiser = svg.getElementById(id);
+        this.radius_fn = radius_fn;
+      }
+      paint(time, value) {
+        if (this.visualiser) {
+          var radius = this.radius_fn(time, value);
+          this.visualiser.setAttribute("r", radius);
+        }
       }
     }
 
@@ -55,12 +119,12 @@ ANIMATION_SCRIPT = '''
         this.visualiser = visualiser;
       }
 
-      draw(from, to) {
+      paint(time_from, time_to) {
         var mx, my;
         var path = "";
         var xy = this.points;
-        var start = this.time_transform.transform(from);
-        var t = this.time_transform.transform(to);
+        var start = this.time_transform.transform(time_from);
+        var t = this.time_transform.transform(time_to);
 
         // Find first point just before `time`
         var i = 0;
@@ -78,61 +142,70 @@ ANIMATION_SCRIPT = '''
             mx = xy[i];
             my = xy[i+1];
           }
-          path = "M " + String(mx) + " " + String(my);
+          if (this.line)
+            path = "M " + String(mx) + " " + String(my);
           if (t > xy[i]) {
             i += 2;
             while (i < xy.length && xy[i] <= t) {
               mx = xy[i]; my = xy[i+1];
               if (i == 2) path += " L";
-              path += " " + String(mx) + " " + String(my);
+              if (this.line)
+                path += " " + String(mx) + " " + String(my);
               i += 2;
             }
             if (i < xy.length) {  // xy[i-2] <= t < xy[i]
               var dt = t - xy[i-2];
               var dy = dt*(xy[i+1]-xy[i-1])/(xy[i]-xy[i-2]);
               mx = t; my = xy[i-1] + dy;
-              path += " l" + String(dt) + " " + String(dy);
+              if (this.line)
+                path += " l" + String(dt) + " " + String(dy);
             }
           }
           if (this.visualiser)
-            this.visualiser(to, this.value_transform.inverse(my));
+            this.visualiser.paint(time_to, this.value_transform.inverse(my));
         }
-        if (path == "") {
-          this.marker.setAttribute("visibility", "hidden");
-        } else if (this.line) {
-          this.line.setAttribute("d", path);
-          this.marker.setAttribute("cx", mx);
-          this.marker.setAttribute("cy", my);
-          this.marker.setAttribute("visibility", "visible");
+        if (this.line) {
+          if (path == "") {
+            this.marker.setAttribute("visibility", "hidden");
+          } else {
+            this.line.setAttribute("d", path);
+            this.marker.setAttribute("cx", mx);
+            this.marker.setAttribute("cy", my);
+            this.marker.setAttribute("visibility", "visible");
+          }
         }
       }
     };
 
     class Animation {
-      constructor(start, end, step) {
+      constructor(start, end) {
         this.start_time = start;
         this.end_time = end;
-        this.time_step = step;
-        this.traces = [];
         this.time = this.start_time;
         this.animation = null;
+        this.traces = [];
+        this.millisecs = 0;
+        this.speed = 1.0;
       }
       add_trace(trace) {
         this.traces.push(trace);
       }
-      draw_traces(time) {
+      paint_traces() {
         for (let trace of this.traces)
-          trace.draw(this.start_time, time);
+          trace.paint(this.start_time, this.time);
       }
       animate() {
-        this.draw_traces(this.time);
-        this.time += this.time_step;
-        if (this.time > this.end_time)
-          this.time = this.start_time;
+        var dt = Date.now() - this.millisecs;
+        this.millisecs = Date.now();
+        this.paint_traces();
+        this.time += dt*this.speed/1000.0;
+        if (this.time > this.end_time) this.time = this.start_time;
       }
-      start(period) {
+      start(speed, period) {
+        this.speed = speed;
         if (this.animation == null)
           this.animation = setInterval(this.animate.bind(this), period);
+        this.millisecs = Date.now();
       }
       stop() {
         if (this.animation != null) {
@@ -142,7 +215,6 @@ ANIMATION_SCRIPT = '''
       }
     };
 '''
-
 
 class Visualiser(object):
     def __init__(self, id, position, setup_js='', call_js=''):
@@ -164,9 +236,11 @@ class Visualiser(object):
     def call_js(self):
         return self._call_js
 
+    def script(self):
+        return ''
+
     def svg(self, plot):
         pass
-
 
 class CircleVisualiser(Visualiser):
     def __init__(self, id, position, fill, **kwds):
@@ -174,24 +248,20 @@ class CircleVisualiser(Visualiser):
         self._fill = fill
 
     def svg(self, plot):
+        ## Add any SVG for self._fill ??
         pos = self.get_pos(plot)
         return ET.Element('circle',
                           dict(id=self._id, cx='%g' % pos[0], cy='%g' % pos[1], r='0', fill=self._fill))
 
+class ChannelVisualiser(Visualiser):
+    def __init__(self, id, position, direction, colour, t, y):
+        super().__init__(id, position)
+        self._direction = direction
+        self._colour = colour
 
-class DiagramVisualiser(Visualiser):
-    def __init__(self, id, t, y):
-
-        ymin = np.min(y)
-        ymax = np.max(y)
-        if ymin == ymax:
-            # Avoid zero division...
-            ymin, ymax = (0, ymax/2.0)
-
-        super().__init__(id, None,
-                         setup_js='var {id}_Var = svg.getElementById("{id}");'.format(id=id),
-                         call_js='(t, y) => {{ var r = 5.0 + 40.0*(y - ({ymin}))/({ymax}-({ymin})); if (r < 0) r = 0; {id}_Var.setAttribute("r", r); }}'
-                                 .format(id=id, ymin=ymin, ymax=ymax))
+        # Scale y values so absolute maximum becomes 1
+        maxabs = y[np.argmax(abs(y))]
+        if maxabs != 0.0: y /= maxabs
 
         xy = np.empty((len(t), 2), dtype=np.float_)
         xy[:, 0] = t
@@ -205,9 +275,19 @@ class DiagramVisualiser(Visualiser):
         # Generate the SVG path, transforming back to the original data values
         self._path = _path.convert_to_string(simplified.transformed(xfm.inverted()), None, None, False, None, 6, [b'M', b'L', b'Q', b'C', b'z'], False).decode('ascii')
 
-    def svg(self, plot):
-        return ET.XML('<defs><path id="{id}_path" d="{d}"/></defs>'.format(id=self._id, d=self._path))
+    def xml(self):
+        transform = ["translate({xpos}, {ypos})".format(xpos=self._position[0], ypos=self._position[1])]
+        if self._direction[-1] == '-':
+            transform.append("rotate(180 0 20)")
+        transform.append("scale(0.2)")
+        return [ET.XML('''<g xmlns:xlink="http://www.w3.org/1999/xlink" transform="{transform}">
+                            <use xlink:href="#IonChannel" fill="{colour}" x="0" id="{id}_channel"/>
+                          </g>'''.format(transform=' '.join(transform), colour=self._colour, id=self._id)),
+                ET.XML('<defs><path id="{id}_data_path" d="{d}"/></defs>'.format(id=self._id, d=self._path))
+                ]
 
+    def script(self):
+        return 'new Trace("{id}_data", null, null, new IonChannel("{id}_channel"))'.format(id=self._id)
 
 class Animation(object):
     def __init__(self, id, trace, display, marker, visualiser):
@@ -218,6 +298,47 @@ class Animation(object):
         self.marker = marker
         self.visualiser = visualiser
 
+class Diagram(object):
+    def __init__(self, diagram_file, simulation, start_time, end_time, units='ms'):
+        if units not in ['ms', 's', 'm', 'h', 'd']:
+            raise ValueError('Unknown timing units')
+        self._svg_tree = ET.parse(diagram_file)
+        self._times = simulation.results().dataStore().voi().values()
+        self._start_index = np.searchsorted(self._times, start_time)
+        self._end_index = np.searchsorted(self._times, end_time) + 1
+        self._variables = simulation.results().dataStore().variables()
+        self._script = [ ]
+        self._script.append(ANIMATION_SCRIPT)
+        self._script.append('var animation = new Animation({start}, {end});'
+                            .format(start=start_time, end=end_time))
+        self._xml = [ ]
+        self._xml.append(ET.XML(DIAGRAM_ANIMATION_SVG))
+
+    def add_channel(self, variable_id, element_id, position, direction, colour):
+        # Get the SVG element
+        element = self._svg_tree.find('//*/svg:g[@id="{id}"]'.format(id=element_id),
+                                      {'svg': 'http://www.w3.org/2000/svg'})
+        # And the corresponding simulation variable
+        variable = self._variables.get(variable_id)
+
+        if element is not None and variable is not None:
+            channel = ChannelVisualiser(element_id, position, direction, colour,
+                                        self._times[self._start_index:self._end_index],
+                                        variable.values()[self._start_index:self._end_index])
+            element.extend(channel.xml())
+            self._script.append('animation.add_trace({trace});'.format(trace=channel.script()))
+
+    def generate_svg(self, speed, period):  #  `period` in milliseconds
+        self._script.append('animation.start({speed}, {period});'.format(speed=speed, period=period))
+        script_element = ET.Element('script', {'type': 'application/ecmascript'})
+        script_element.text = '<![CDATA[{code}]]>'.format(code='\n'.join(self._script))
+        self._xml.append(script_element)
+
+        self._svg_tree.getroot().extend(self._xml)
+
+        # Return SVG as a Unicode string. A method of 'html' ensures
+        # that '<', '>' and '&' are not escaped.
+        return ET.tostring(self._svg_tree, encoding='unicode', method='html')
 
 class Figure(matplotlib.figure.Figure):
     def __init__(self, **kwds):
@@ -231,6 +352,7 @@ class Figure(matplotlib.figure.Figure):
         plt.close(self)
 
     def add_animation(self, trace, id=None, display='animate', marker=True, timing=None, visualiser=None):
+# Use trace to get range etc of visualiser...
         if display in [None, 'hidden', 'visible'] and not marker and visualiser is None:
             raise ValueError('Nothing to animate...')
         if id is None:
@@ -253,8 +375,7 @@ class Figure(matplotlib.figure.Figure):
                 .scale(1.0, -1.0)
                 .translate(0.0, POINTS_PER_INCH*self.get_figheight()))
 
-
-    def generate_svg(self, start, end, step, units='ms', speed=1.0, period=10, **kwds):  ## Period in ms
+    def generate_svg(self, start, end, step, units='ms', speed=1.0, **kwds):  ## Period in ms
         if units not in ['ms', 's', 'm', 'h', 'd']:
             raise ValueError('Unknown timing units')
 
@@ -282,12 +403,20 @@ class Figure(matplotlib.figure.Figure):
 
             path = svg_tree.find('.//svg:g/svg:g/svg:g[@id="{id}"]/svg:path'.format(id=animation.id),
                                  {'svg': 'http://www.w3.org/2000/svg'})
+
             d = path.get('d')
             self.add_xml(ET.XML('<defs><path id="{id}_path" d="{d}"/></defs>'.format(id=animation.id, d=d)))
+            '''
+            p = path.find('../..')
+            p.getparent().remove(p)
+'''
             # Affine matrix to transform world coordinates into SVG point-based coordinates
             xfm = self.make_flip_transform(plot.transData).get_matrix()
+
             if animation.visualiser:
+## Where we can auto-calculate visualisers...
                 self.add_xml(animation.visualiser.svg(plot))
+## Replace by "new Visualiser(id)"
                 self.add_script(animation.visualiser.setup_js())
                 invoke_visualiser = ', %s' % animation.visualiser.call_js()
             else:
@@ -311,82 +440,29 @@ class Figure(matplotlib.figure.Figure):
 
         return ET.tostring(svg_tree, encoding='unicode', method='html')
 
-
 def figure(**kwds):
     return plt.figure(FigureClass=Figure, tight_layout=True, **kwds)
-
-
-def animate_diagram(diagram, simulation, element_map, start, end, step, units='ms', speed=1.0, period=1):  ## Period in ms
-    if units not in ['ms', 's', 'm', 'h', 'd']:
-        raise ValueError('Unknown timing units')
-
-    ## Allow for units and scale everything?? Adjust speed ???
-    step *= speed
-
-    javascript = [ ]
-    javascript.append(ANIMATION_SCRIPT)
-    javascript.append('var animation = new Animation(%g, %g, %g);' % (start, end, step))
-
-    animation_xml = [ ]
-    visualisers = [ ]
-
-
-    variables = simulation.results().dataStore().variables()
-
-    t = simulation.results().dataStore().voi().values()
-    n_start = np.searchsorted(t, start)
-    n_end = np.searchsorted(t, end) + 1
-
-    svg_tree = ET.parse(diagram)
-
-    for variable_id, element_id in element_map.items():
-        # Find the ID of the animation element's radialGradient
-# TTusscher        gradient = svg_tree.find('//*/svg:g[@id="{id}"]/svg:g/svg:g/svg:g/svg:radialGradient'.format(id=element_id),
-        gradient = svg_tree.find('//*/svg:g[@id="{id}"]/svg:g/svg:g/svg:radialGradient'.format(id=element_id),
-                                 {'svg': 'http://www.w3.org/2000/svg'})
-        # And the corresponding variable
-        variable = variables.get(variable_id)
-
-        if gradient is not None and variable is not None:
-            gradient_id = gradient.get('id')
-            visualisers.append(DiagramVisualiser(gradient_id, t[n_start:n_end], variable.values()[n_start:n_end]))
-
-    for visualiser in visualisers:
-        animation_xml.append(visualiser.svg(None))
-        javascript.append(visualiser.setup_js())
-        javascript.append('animation.add_trace(new Trace("%s", null, null, %s));'
-                                            % (visualiser.id, visualiser.call_js()))
-
-    javascript.append('animation.start(%g);' % period)
-
-    script_element = ET.Element('script', {'type': 'application/ecmascript'})
-    script_element.text = '<![CDATA[%s]]>' % '\n'.join(javascript)
-    animation_xml.append(script_element)
-
-    svg_tree.getroot().extend(animation_xml)
-
-    # Return SVG as a Unicode string. A method of 'html' ensures
-    # that '<', '>' and '&' are not escaped.
-
-    return ET.tostring(svg_tree, encoding='unicode', method='html')
 
 
 if __name__ == '__main__':
 
     import OpenCOR as oc
-
+    '''
     s = oc.openSimulation('noble_1962.cellml')
     s.data().setEndingPoint(0.6)
     s.data().setPointInterval(0.005)
     s.reset()
     s.run()
+'''
+    s = oc.simulation()
 
-    svg = animate_diagram('noble_1962.svg', s, {'sodium_channel/i_Na': 'i_Na',
-                                                'potassium_channel/i_K': 'i_K',
-                                                'leakage_current/i_Leak': 'i_Leak'},
-                                                0.04, 0.60, 0.01, speed=0.2)
-##i_Na
-##  animation.add_trace(new Trace("SVGID_2_", null, null, (t, y) => { var r = -50.0 + 100.0*(y - (-148369.27383164677))/(-8807.722189250824-(-148369.27383164677)); if (r < 0) r = 0; SVGID_2__Var.setAttribute("r", r); }));
+    diagram = Diagram('noble_1962.svg', s, 0.04, 0.60, 's')
+
+    diagram.add_channel('sodium_channel/i_Na', 'i_Na', (183.849, 49.756), 'V', 'red')
+    diagram.add_channel('potassium_channel/i_K', 'i_K', (80.867, 49.756), 'V-', '#b666d2')
+    diagram.add_channel('leakage_current/i_Leak', 'i_Leak', (133.551, 167.485), 'V', 'orange')
+
+    svg = diagram.generate_svg(speed=0.5, period=25)
 
     browser = oc.browserWebView()
     browser.setContent(svg, "image/svg+xml")
@@ -452,18 +528,21 @@ else:
       </radialGradient>
     </defs>'''))
 
-    sodium_visualiser = CircleVisualiser('sodium', (0.9, 0.8),
+    sodium_visualiser = CircleVisualiser('sodium', (0.95, 0.8),
                                           'url(#RedGradient)',
                                           setup_js='var sodiumVar = svg.getElementById("sodium");',
 #                                          call_js= '(t, y) => { voltageVar.setAttribute("r", 5*(y-3)); }')
+## Get range and use this to set default function for radius...
                                           call_js='(t, y) => { sodiumVar.setAttribute("r", 5*(y-2)); }')
 #                                          call_js='(t, y) => { sodiumVar.setAttribute("r", 600*(y - 8.59)); }')
+
 
     sodium_plot = fig.add_subplot(211, xlabel='Time (ms)', ylabel='Na i (millimolar)')
     sodium_trace = sodium_plot.plot(t, na, lw=1)[0]
     fig.add_animation(sodium_trace, visualiser=sodium_visualiser)
+                                 #  visualiser=CircleVisualiser(colour=??))
 
-    voltage_visualiser = CircleVisualiser('voltage', (0.9, 0.8),
+    voltage_visualiser = CircleVisualiser('voltage', (0.95, 0.8),
                                           'url(#RedGradient)',
                                           setup_js='var voltageVar = svg.getElementById("voltage");',
                                           call_js='(t, y) => { voltageVar.setAttribute("r", 5*(y+1)); }')
@@ -475,7 +554,13 @@ else:
 
 ## Auto determine max T and speed
     svg = fig.generate_svg(0, 10, 1, speed=0.1)
+## 600, 0.5
     fig.close()
+
+
+#    f = open('test.svg', 'w')
+#    f.write(svg)
+#    f.close()
 
     browser = oc.browserWebView()
     browser.setContent(svg, "image/svg+xml")
