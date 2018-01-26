e830059f76342176dcf82baae50bc9c93323ffce
David Brooks <dave@bcs.co.nz>
2018-01-27 11:47:58 +1300

    Tweak bond graph drawing code and move out of Diagram class.

--- cell_diagram.py
+++ cell_diagram.py
@@ -70,16 +70,6 @@ class CellDiagram(Container):
     def add_flow(self, flow):
         self._flows.append(flow)
 
-    def bond_graph(self):
-        g = igraph.Graph()
-        g.add_vertices(len(self._potentials))
-        potentials = list(self._potentials.keys())
-        edges = []
-        for flow in self._flows:
-            for flux in flow.fluxes:
-                edges.append((potentials.index(flux.from_potential), potentials.index(flux.to_potential)))
-        g.add_edges(edges)
-        return g
     def add_potential(self, potential, quantity):
         self._potentials[potential] = quantity
 
@@ -298,6 +288,21 @@ def parse(file, stylesheet=None):
 
 #------------------------------------------------------------------------------
 
+def bond_graph(diagram):
+    g = igraph.Graph(directed=True)
+    potentials = list(diagram.potentials.keys())
+    g.add_vertices(potentials)
+    g.vs['label'] = potentials
+    for flow in diagram.flows:
+        g.add_vertex(flow.id, label=flow.id, color='blue')
+        for flux in flow.fluxes:
+            for n in range(flux.count):
+                g.add_edge(flux.from_potential, flow.id)
+                g.add_edge(flow.id, flux.to_potential)
+    return g
+
+#------------------------------------------------------------------------------
+
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.DEBUG)
 
@@ -330,7 +335,7 @@ if __name__ == '__main__':
 
     ## parse('cell_diagram.xml', stylesheet)
 
-    bond_graph = diagram.bond_graph()
-    layout = bond_graph.layout("kk")
-    igraph.plot(bond_graph, layout=layout)
+    g = bond_graph(diagram)
+    layout = g.layout("kk")
+    igraph.plot(g, layout=layout)
 
