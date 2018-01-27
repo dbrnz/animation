e0a969673cc0dccc0d77c9585ab4d24010b4db8e
David Brooks <dave@bcs.co.nz>
2018-01-27 13:39:13 +1300

    Manually specify graph layout using a simple example.

--- diagram_test.py
+++ diagram_test.py
@@ -18,13 +18,18 @@ def bond_graph(diagram):
     g = igraph.Graph(directed=True)
     potentials = list(diagram.potentials.keys())
     g.add_vertices(potentials)
+    g.vs['color'] = 'green'
     g.vs['label'] = potentials
+    for p in potentials:
+        q = p.replace('u', 'q')
+        g.add_vertex(q, label=q)
+        g.add_edge(p, q)
     for flow in diagram.flows:
         g.add_vertex(flow.id, label=flow.id, color='blue')
         for flux in flow.fluxes:
             for n in range(flux.count):
                 g.add_edge(flux.from_potential, flow.id)
-                g.add_edge(flow.id, flux.to_potential)
+                for p in flux.to_potential.split(): g.add_edge(flow.id, p)
     return g
 
 #------------------------------------------------------------------------------
@@ -57,14 +62,18 @@ if __name__ == '__main__':
         }
       ''')
 
+##  parse('cell_diagram.xml', stylesheet)
+##  diagram = parse('bond_graph.xml')
 
-
-    diagram = parse('bond_graph.xml')
-
-    ## parse('cell_diagram.xml', stylesheet)
+    diagram = parse('atp.xml')
 
     g = bond_graph(diagram)
-    layout = g.layout("kk")
-    igraph.plot(g, layout=layout)
+    g.vs['x'] = [0,  2,   2,     0,    2,   2,   1]
+    g.vs['y'] = [0, -0.2, 0.2,  -0.1, -0.3, 0.3, 0]
+    layout = g.layout("auto")
+    print(g)
+    for l in layout:
+        print(l)
+    igraph.plot(g, layout=layout) ##, bbox=(200, 100)) ## , target='atp.svg')
 
 #------------------------------------------------------------------------------
